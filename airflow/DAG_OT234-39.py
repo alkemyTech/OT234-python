ea3f4e0f0aa09d91d8a681db10cfa27ff0e761b8
Wquiroz2022 <vvhidalgo@hotmail.om>
2022-06-22 00:37:07 -0300

    configure log

--- airflow/DAG_OT234-39.py
+++ airflow/DAG_OT234-39.py
@@ -1,10 +1,21 @@
 "Configurar un DAG, sin consultas, ni procesamiento. Para Hacer un ETL para 2 universidades distintas."
+'Configurar los log'
 
 from datetime import timedelta, datetime
 
 from airflow import DAG
 
 from airflow.operators.python_operator import PythonOperator
+
+import logging
+
+
+# configuration logging
+logging.basicConfig(
+level=logging.DEBUG,
+format='%(asctime)s-%(levelname)s-%(message)s', datefmt='%Y/%m/%d' 
+    )
+Logger=logging.Debug('starting the DAG')
 
 # dag configuration
 default_args={
@@ -15,6 +26,7 @@ default_args={
     'retries': 5,
     'retry_delay': timedelta(hours=1)
 }
+Logger=logging.DEBUG('COmenzando el DAG')
 
 with DAG('Consulta universidades', 
           description='Consulta a la UNiversidad del Comahue',
@@ -23,11 +35,15 @@ with DAG('Consulta universidades',
           default_args=default_args,
           template_searchpath='\Users\WalterPc\Documents\OT234-python\include', 
          ) as dag:
-         
+
+        Logger=logging.DEBUG('Querying the database, Universidad NAcional de Comahue')    
+
         UNCo_query=PythonOperator(
             task_id='UNCo_query',
             sql='UNCo_2020-09-01_2021-02-01.sql'
             )
+
+        Logger=logging.DEBUG('Querying the database, Universidad del Salvador')
 
         UNCo_query=PythonOperator(
             task_id='USal_query',
