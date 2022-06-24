9b24ce7c331b3e9e60d54125b3097629da234852
Javier Vera <jvera.studio@gmail.com>
2022-06-24 17:27:54 -0300

    UFLO and UNVM dag template finished

--- airflow/dags/UFLo_UNVM_dag.py
+++ airflow/dags/UFLo_UNVM_dag.py
@@ -2,6 +2,7 @@ from datetime import timedelta, datetime
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 
 """
 A futuro probablemente usaré:
@@ -10,17 +11,54 @@ A futuro probablemente usaré:
      un python operator que ejecute funciones para amazon cloud --> Subida de archivos a amazon. 
 """
 
+def dummy():
+    return True
+
+default_args={
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(hours=1)
+}
+
 with DAG(
     'UNTF_2020-09-01_2021-02-01',
     description='dag para la univesidad UNTF',
     schedule_interval=timedelta(hours=1),
-    start_date=datetime(2022,6,16)
+    start_date=datetime(2022,6,16),
+    default_args=default_args,
+
 ) as dag:
-    t1 = EmptyOperator(task_id)(
-        task_id='task_1',
+    UFLo_query = EmptyOperator(
+        task_id = 'UFLo_query'
         )
 
-t1
+    UFLo_transform = PythonOperator(
+        task_id='UFLo_transform',
+        python_callable=dummy
+        )
+
+    UFLo_amazon = EmptyOperator(
+        task_id='UFLo_amazon',
+        )
+
+    UNVM_query = EmptyOperator(
+        task_id = 'UNVM_query'
+        )
+
+    UNVM_transform = PythonOperator(
+        task_id='UNVM_transform',
+        python_callable=dummy
+        )
+
+    UNVM_amazon = EmptyOperator(
+        task_id='UNVM_amazon',
+        )
+
+#UFLo_query >> UFLo_transform >> UFLo_amazon
+#UNVM_query >> UNVM_transform >> UNVM_amazon
 
     
    
