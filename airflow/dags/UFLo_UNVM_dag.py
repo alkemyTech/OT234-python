d6c9a1fbc458badb2d3042343998139392766196
Barrilete Com <jvera.studio@gmail.com>
2022-06-27 14:09:32 -0300

    OT234-63 (#16)
    * archivos sql terminados
    
    * UFLo_UNVM dag template
    
    * UFLO and UNVM dag template finished
    
    * SQL operators terminados
    
    * SQL operators terminados

--- airflow/dags/UFLo_UNVM_dag.py
+++ airflow/dags/UFLo_UNVM_dag.py
@@ -4,12 +4,22 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 
-"""
-A futuro probablemente usaré:
-     un custom operator (datasourcetocsv) --> Extracción desde el servidor y creación de csv
-     un python operator que ejecute diferentes funciones de python --> Transformación de los datasets
-     un python operator que ejecute funciones para amazon cloud --> Subida de archivos a amazon. 
-"""
+from datasourcetocsv_operator import DataSourceToCsvOperator
+
+from datetime import datetime
+
+now = datetime.now()
+current_time = now.strftime("%d/%m:%y")
+
+dataset_dir = "/home/jvera/gitRepos/OT234-python/airflow/datasets/"
+sql_dir = "/home/jvera/gitRepos/OT234-python/airflow/include/"
+
+def sqlFileToQuery(path):
+    query = ''
+    with open(path) as file:
+        for line in file:
+            query += line
+    return query
 
 def dummy():
     return True
@@ -31,9 +41,13 @@ with DAG(
     default_args=default_args,
 
 ) as dag:
-    UFLo_query = EmptyOperator(
-        task_id = 'UFLo_query'
-        )
+    UFLo_query = DataSourceToCsvOperator(
+        task_id='UFLO_query',
+        sql= sqlFileToQuery(sql_dir + "UNTF_2020-09-01_2021-02-01_OT234-12.sql"),
+        postgres_conn_id="postgres_server",
+        database="training",
+        output_dir= dataset_dir + 'UFLo_2020-09-01_2021-02-01.csv'
+        #output_dir= dataset_dir + 'UFLo_2020-09-01_2021-02-01-{}.csv'.format(current_time)
 
     UFLo_transform = PythonOperator(
         task_id='UFLo_transform',
@@ -44,9 +58,15 @@ with DAG(
         task_id='UFLo_amazon',
         )
 
-    UNVM_query = EmptyOperator(
-        task_id = 'UNVM_query'
-        )
+
+    UNVM_query = DataSourceToCsvOperator(
+        task_id='UNVM_query',
+        sql=sqlFileToQuery(sql_dir + "UTNa_2020-09-01_2021-02-01_OT234-12.sql"),
+        postgres_conn_id="postgres_server",
+        database="training",
+        output_dir= dataset_dir + 'UTNa_2020-09-01_2021-02-01.csv'
+        # output_dir= dataset_dir + 'UTNa_2020-09-01_2021-02-01-{}.csv'.format(current_time)
+
 
     UNVM_transform = PythonOperator(
         task_id='UNVM_transform',
@@ -57,8 +77,11 @@ with DAG(
         task_id='UNVM_amazon',
         )
 
-#UFLo_query >> UFLo_transform >> UFLo_amazon
-#UNVM_query >> UNVM_transform >> UNVM_amazon
+
+UFLo_query >> UFLo_transform >> UFLo_amazon
+
+UNVM_query >> UNVM_transform >> UNVM_amazon
+
 
     
    
---
+++ airflow/plugins/datasourcetocsv_operator.py
@@ -0,0 +1,64 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from datetime import datetime, timedelta
+from os import environ
+import csv
+
+
+
+
+
+class DataSourceToCsvOperator(BaseOperator):
+    """
+    Extract data from the data source to CSV file
+    """
+
+    template_fields = ('sql',)
+    template_ext = ('.sql',)
+    ui_color = '#ededed'
+
+    
+    def __init__(
+            self, sql,
+            postgres_conn_id='postgres_default', autocommit=False,
+            parameters=None,
+            database=None,
+            output_dir='/tmp/',
+            *args, **kwargs):
+        super(DataSourceToCsvOperator, self).__init__(*args, **kwargs)
+        self.sql = sql
+        self.postgres_conn_id = postgres_conn_id
+        self.autocommit = autocommit
+        self.parameters = parameters
+        self.database = database
+        self.file_path = output_dir
+
+    def execute(self, context):
+        self.log.info('Executing: %s', self.sql)
+        self.hook = PostgresHook(postgres_conn_id=self.postgres_conn_id,
+                                 schema=self.database)
+        conn = self.hook.get_conn()
+        cursor = conn.cursor()
+        #self.hook.run(self.sql, self.autocommit, parameters=self.parameters)
+        cursor.execute(self.sql)
+        result = cursor.fetchall()
+        # Write to CSV file
+        # temp_path = self.file_path + '_dump_.csv'
+        # tmp_path = self.file_path + 'dump.csv'
+
+        temp_path = self.file_path
+        tmp_path = self.file_path 
+
+        print(temp_path,tmp_path)
+
+        with open(temp_path, 'w') as fp:
+            a = csv.writer(fp, quoting = csv.QUOTE_MINIMAL, delimiter = '|')
+
+            a.writerow([i[0] for i in cursor.description])
+            a.writerows(result)
+        #full_path = temp_path + '.gz'
+        with open(temp_path, 'rb') as f:
+            data = f.read()
+        f.close()
+        # self.hook.bulk_dump(self.sql,tmp_path)
