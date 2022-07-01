ea12d71d8c1961f161dd922bbd5d99b336eabd0b
Sergio Miranda <89145924+sergeimiranda@users.noreply.github.com>
2022-07-01 12:42:10 -0300

    Ot234 62 (#30)
    * SQL Operator
    
    * OT234-64 Extraction
    
    * OT234-62

---
+++ airflow/dags/dag_OT234_UPal.py
@@ -0,0 +1,81 @@
+# DAG sin consultas ni procesamiento para el grupo de universidades C
+# Universidad de Palermo
+
+from airflow import DAG
+from datetime import timedelta, datetime
+import pandas as pd
+import psycopg2
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.hooks.postgres_hook import PostgresHook
+import logging
+import csv
+import os
+import sys
+
+
+# Work directory and path to files
+cwd = os.getcwd()
+# Import modules from paths
+sys.path.append(cwd + '/plugins/')
+from query_to_csv import queryTOcsv
+
+
+
+#############Create logger
+logger = logging.getLogger('Universidad de Palermo')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s', datefmt='%Y-%m-%d')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+logger.info('Inicio')
+
+############# Arguments Configuration
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1)
+}
+#############
+
+############# PARAMETERS 
+sql_query = cwd + '/Include/UPal_2020-09-01_2021-02-01_OT234-14.sql'
+raw_path = cwd + '/files/'
+Univ='UPal'
+
+#############
+
+with DAG(
+    dag_id='dag_OT234_UPal',
+    description='Dag procesamiento Universidad de Palermo',
+    default_args=default_args,
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2022,6,27),
+    max_active_runs=3,
+    catchup=False
+
+)  as dag:
+    Query_Palermo=PythonOperator(
+    task_id='Query_Universidad-de-Palermo',
+    python_callable=queryTOcsv,
+    op_args={Univ, raw_path, sql_query},
+    dag=dag,
+    )
+    
+    Query_Palermo
