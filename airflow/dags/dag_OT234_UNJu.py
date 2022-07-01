941812c5facd2b8103dee9387a5eb0279247b810
Sergio Miranda <sergeimiranda@hotmail.com>
2022-07-01 12:32:05 -0300

    OT234-62

--- airflow/dags/dag_OT234_UNJu.py
+++ airflow/dags/dag_OT234_UNJu.py
@@ -5,13 +5,26 @@ from airflow import DAG
 from datetime import timedelta, datetime
 import pandas as pd
 import psycopg2
+from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.hooks.postgres_hook import PostgresHook
 import logging
+import csv
+import os
+import sys
 
 
-##### Create logger
+# Work directory and path to files
+cwd = os.getcwd()
+# Import modules from paths
+sys.path.append(cwd + '/plugins/')
+from query_to_csv import queryTOcsv
+
+
+
+############# Create logger
 logger = logging.getLogger('Universidad Nacional de Jujuy')
 logger.setLevel(logging.DEBUG)
 
@@ -28,52 +41,42 @@ ch.setFormatter(formatter)
 # add ch to logger
 logger.addHandler(ch)
 
-# 'application' code
-#logger.debug('debug message')
-logger.info('Mensaje base de datos')
-#logger.warning('warn message')
-#logger.error('error message')
-#logger.critical('critical message')
+logger.info('Inicio')
 
-#######
-
-
-#Conección Base de datos (incompleta)
-HOST_id='training-main.cghe7e6sfljt.us-east-1.rds.amazonaws.com'
-
-
-###Configuración Retries
+############# Arguments Configuration
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 5,
-    'retry_delay': timedelta(minutes=10)
+    'retry_delay': timedelta(minutes=1)
 }
-######
+#############
 
+############# PARAMETERS 
+sql_query = cwd + '/Include/UNJu_2020-09-01_2021-02-01_OT234-14.sql'
+raw_path = cwd + '/files/'
+Univ='UNJu'
 
-with DAG('dag_OT234-30_UNJu',
-    description='Dag procesamiento universidad Nacional de Jujuy',
+#############
+
+with DAG(
+    dag_id='dag_OT234_UNJu',
+    description='Dag procesamiento Universidad Nacional de Jujuy',
+    default_args=default_args,
     schedule_interval=timedelta(hours=1),
-    start_date=datetime(2022,6,20),
-    template_searchpath='/OT234-python/airflow/include',
+    start_date=datetime(2022,6,27),
+    max_active_runs=3,
+    catchup=False
 
-) as dag:
+)  as dag:
+    Query_Jujuy=PythonOperator(
+    task_id='Query_Universidad-Nacional-de-Jujuy',
+    python_callable=queryTOcsv,
+    op_args={Univ, raw_path, sql_query},
+    dag=dag,
+    )
     
-    Query_Jujuy = DummyOperator(
-        task_id='Query_Jujuy',
-    #    postgres_conn_id=HOST_id,
-    #    sql='UNJu_2020-09-01_2021-02-01_OT234-14.sql'
-        )
-    
-    Proc_Pandas = DummyOperator(
-        task_id='Proc_Pandas',
-        )
-
-    Upload_S3 = DummyOperator(
-        task_id='Upload_S3',
-        )
-
-    Query_Jujuy >> Proc_Pandas >> Upload_S3
+    Query_Jujuy
+   
