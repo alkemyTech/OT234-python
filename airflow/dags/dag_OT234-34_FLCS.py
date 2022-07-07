5e1fbbc4c3bdc3ca1d6f1e530016d1e743f4ae80
Tomas <tomas.r3@outlook.com>
2022-07-07 00:16:37 -0300

    OT234-50

--- airflow/dags/dag_OT234-34_FLCS.py
+++ airflow/dags/dag_OT234-34_FLCS.py
@@ -6,11 +6,37 @@ Facultad Latinoamericana De Ciencias Sociales
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+import logging
 
 #Se usaran en un futuro: 
 # postgres operator para la extraccion de datos
 # python operator para la transformacion
-# python operator para la carga de datos                  
+# python operator para la carga de datos                 
+
+
+# Logging configuration
+# creo el logger
+logger = logging.getLogger('DAG_logger')
+logger.setLevel(logging.DEBUG)
+
+# formato del logger
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s', '%Y-%m-%d')
+
+# defino el console handler y seteo el level a DEBUG
+#Level DEBUG se utiliza para informacion detallada, usualmente de nuestro interes
+#para diagnosticar problemas
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+
+# defino el file handler y seteo el level a DEBUG
+fh = logging.FileHandler('dag_OT234-34_FLCS.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
+
 
 default_args={
     'owner': 'airflow',
@@ -26,7 +52,9 @@ with DAG(
     description='DAG ETL para Facultad Latinoamericana De Ciencias Sociales',
     schedule_interval=timedelta(hours=1),
     start_date=datetime.today(),
-    default_args=default_args
+    default_args=default_args,
+    template_searchpath='/home/tomasreuque/Desktop/OT234-python/airflow/dags/dag_OT234-34_FLCS.py',
+    catchup=False
 ) as dag:
 
     extract= DummyOperator(task_id='extract')
