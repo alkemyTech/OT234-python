2ee3d4122b8f7fdc37a5ea2c8dc9c4f9848e3c4b
nachomachado <ignmachado@gmail.com>
2022-07-13 18:27:47 +0200

    Created functions and scripts to process the dataset. When the script OT234-136.py runs It prints all results ordered.

--- bigdata/OT234-136.py
+++ bigdata/OT234-136.py
@@ -1,9 +1,10 @@
 from typing import Counter
 from functools import reduce
 from lib.chunckify import chunckify
-from lib.add_timer import get_duration
+from lib.OT234_136_functions import get_duration
 from lib.OT234_136_functions import get_dates, reducir_contadores, mapper_1
 from lib.OT234_136_functions import get_answer_visit, get_post_type_1, get_ratio, mapper_2
+from lib.OT234_136_functions import mapper_3, posts_reducer, reducer, get_answer_time
 import xml.etree.ElementTree as ET
 from pathlib  import Path
 import copy
@@ -12,23 +13,29 @@ PARENT_PATH = Path(__file__).parent.absolute()
 
 @get_duration
 def map_reduce_1(data_chunks):
-    data= data_chunks
-    mapped = map(mapper_1, data)
+    mapped = map(mapper_1, data_chunks)
     counted = map(Counter, mapped)
-    reduced = reduce(reducir_contadores, counted).most_common(10)
-    print(reduced)
+    result_1 = reduce(reducir_contadores, counted).most_common(10)
+    print(result_1)
 
 @get_duration
 def map_reduce_2(data_chunks):
-    data= data_chunks
-    mapped = list(map(mapper_2, data_chunks))
-    print(mapped)
+    result_2 = list(map(mapper_2, data_chunks))
+    print(result_2)
 
 @get_duration
 def map_reduce_3(data_chunks):
-    data= data_chunks
+    dates_mapped = list(map(mapper_3, data_chunks))
+    unchunk = list(reduce(reducer, dates_mapped))
+    reduced = dict(reduce(posts_reducer, unchunk))
+    duration_reduce = list(map(get_answer_time, reduced.values()))
+    filtered = list(filter(None, duration_reduce))
+    size  = len(filtered)
+    result_3 = reduce(lambda x,y: x+y, filtered) / size
+    print(result_3)
 
 if '__main__' == __name__:
+    
     tree = ET.parse(PARENT_PATH.joinpath('datasets').joinpath('posts.xml'))
     root = tree.getroot()
     data_chunks_1 = chunckify(root, 50)
@@ -38,3 +45,5 @@ if '__main__' == __name__:
     map_reduce_1(data_chunks_1)
     map_reduce_2(data_chunks_2)
     map_reduce_3(data_chunks_3)
+    
+    
--- bigdata/lib/OT234_136_functions.py
+++ bigdata/lib/OT234_136_functions.py
@@ -1,6 +1,15 @@
 from typing import Counter
 from datetime import datetime
 
+def get_duration(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        func(*args, **kwargs)
+        end = datetime.now()
+        time = end - start
+        print('Took ', time.total_seconds(), ' s')
+    return wrapper
+
 # Enunciado 1
 
 def get_dates(data):
@@ -40,3 +49,43 @@ def mapper_2(data):
     resp_visit = list(map(get_answer_visit, filtered))
     ratio = list(map(get_ratio, resp_visit))
     return ratio
+
+# Enunciado 3
+
+def get_creation_date(data):
+    date = datetime.strptime(data.attrib['CreationDate'], '%Y-%m-%dT%H:%M:%S.%f')
+    if int(data.attrib['Score']) > 100:
+        return None
+    elif data.attrib['PostTypeId'] ==  '1':
+        return {data.attrib['Id']:[date, None]}
+    elif data.attrib['PostTypeId'] ==  '2':
+        return {data.attrib['ParentId']: [None, date]}
+
+def mapper_3(data):
+    mapped = list(map(get_creation_date, data))
+    mapped = list(filter(None, mapped))
+    return mapped
+
+def reducer(data1, data2):
+    data1.extend(data2)
+    return data1
+
+def posts_reducer(data1, data2):
+    data_1 = data1.copy()
+    data_2 = data2.copy()
+    key_2 = list(data_2.keys())[0]    
+    if data_2[key_2][1] == None or data_1.get(key_2) == None:
+        data_1.update(data_2)
+        return data_1
+    elif data_1[key_2][1] == None:
+        data_1[key_2][1] = data_2[key_2][1]
+        return data_1
+    elif data_1[key_2][1] < data_2[key_2][1]:
+        data_1[key_2][1] = data_2[key_2][1]
+    return data_1
+
+def get_answer_time(data):
+    try: 
+        return (data[1]-data[0]).total_seconds()
+    except TypeError:
+        return None
