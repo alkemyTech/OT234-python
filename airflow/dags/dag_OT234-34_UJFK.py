620c9cbcb51be6664cc769e1df90862371b496ec
Tomas Reuque <tomas.r3@outlook.com>
2022-07-18 13:32:31 -0300

    OT234 50 (#43)
    * OT234-34
    
    * operador actualizado a dummy
    
    * OT234-80 (#19)
    
    * Created .sql files to solve issue OT234-16
    
    * DAGS created to solve issues OT234-32, -40 and -48.
    
    * Names changed
    
    * Files moved from OT234-PYTHON/. to OT234-PYTHON/airflow/.
    
    * Added extract task using a PythonOperator. It is working downloading data in a .csv file
    
    * Takes extracted data and saves processed data into /airflow/processed_csv. It does not process data
    
    * Changed where the logs are saved.
    
    * Modified path
    
    * Files structure solved
    
    * Added functions to process data importing custom modules
    
    * Fixed files structure
    
    * Added logging events.
    
    * Added PythonOperator to transform data and logging events.
    
    * Changed names functions and added return in exception.
    
    * Developed data transformation. Moved module from /files/plugins/utils to /plugins
    
    * Finshed data transformation according to issue OT234-80. Full processing steps were developed in /airflow/plugins/data_transformation_functions.py
    
    * Ot234 62 (#30)
    
    * SQL Operator
    
    * OT234-64 Extraction
    
    * OT234-62
    
    * OT234-92 (#20)
    
    * Created .sql files to solve issue OT234-16
    
    * DAGS created to solve issues OT234-32, -40 and -48.
    
    * Names changed
    
    * Files moved from OT234-PYTHON/. to OT234-PYTHON/airflow/.
    
    * Added extract task using a PythonOperator. It is working downloading data in a .csv file
    
    * Takes extracted data and saves processed data into /airflow/processed_csv. It does not process data
    
    * Changed where the logs are saved.
    
    * Modified path
    
    * Files structure solved
    
    * Added functions to process data importing custom modules
    
    * Fixed files structure
    
    * Added logging events.
    
    * Added PythonOperator to transform data and logging events.
    
    * Changed names functions and added return in exception.
    
    * Developed data transformation. Moved module from /files/plugins/utils to /plugins
    
    * Finshed data transformation according to issue OT234-80. Full processing steps were developed in /airflow/plugins/data_transformation_functions.py
    
    * Upload file to S3. Was developed a function with a PythonOperator that takes the dataset and upload to S3.
    
    * Changed name file for try-exception
    
    * Added absolute path feature
    
    * OT234-18 (#27)
    
    * OT234-34
    
    * OT234-18
    
    * OT234-18
    
    * OT234-18
    
    * OT234-18
    
    * OT234-18
    
    * OT234-18
    
    * OT234-34 (#24)
    
    * Ot234 78 (#31)
    
    * OT234-78 Commit
    
    * OT234-78
    
    * OT234-79 (#18)
    
    * archivos sql terminados
    
    * UFLo_UNVM dag template
    
    * UFLO and UNVM dag template finished
    
    * SQL operators terminados
    
    * SQL operators terminados
    
    * python operator for transforming to txt
    
    * python operater for transforming to txt
    
    * aws upload operator finished
    
    * se agrega los DAGS para Universidad de Flores y Universidad Nacional de Villa Maria (#22)
    
    * operador actualizado a dummy
    
    * OT234-34
    
    * OT234-42
    
    * OT234-42
    
    * OT234-42
    
    * OT234-42
    
    * fixed
    
    * OT234-42
    
    * OT234-50
    
    * fixed wrong paths and colflicts
    
    Co-authored-by: Ignacio Machado <ignmachado@gmail.com>
    Co-authored-by: Sergio Miranda <89145924+sergeimiranda@users.noreply.github.com>
    Co-authored-by: Javier Vera <jvera.studio@gmail.com>
    Co-authored-by: Leonel Godoy <60699907+aka-leonel@users.noreply.github.com>

--- airflow/dags/dag_OT234-34_UJFK.py
+++ airflow/dags/dag_OT234-34_UJFK.py
@@ -5,15 +5,37 @@ ni procesamiento para la universidad Universidad J. F. Kennedy
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+import logging
 
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 #Se usaran en un futuro: 
 # postgres operator para la extraccion de datos
 # python operator para la transformacion
 # python operator para la carga de datos
+
+# Logging configuration
+# creo el logger
+logger = logging.getLogger('DAG_logger')
+logger.setLevel(logging.DEBUG)
+
+# formato del logger
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s', '%Y-%m-%d')
+
+# defino el console handler y seteo el level a DEBUG
+#Level DEBUG se utiliza para informacion detallada, usualmente de nuestro interes
+#para diagnosticar problemas
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+
+# defino el file handler y seteo el level a DEBUG
+fh = logging.FileHandler('dag_OT234-34_UJFK.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+
 
 default_args={
     'owner': 'airflow',
@@ -29,7 +51,10 @@ with DAG(
     description='DAG ETL para Universidad J. F. Kennedy',
     schedule_interval=timedelta(hours=1),
     start_date=datetime.today(),
-    default_args=default_args
+    default_args=default_args,
+    template_searchpath='/home/tomasreuque/Desktop/OT234-python/airflow/include',
+    catchup=False
+
 ) as dag:
 
     extract= DummyOperator(task_id='extract')
