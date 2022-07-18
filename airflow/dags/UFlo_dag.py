b518e2149d38549b3a547ff8afbe18d7a403293f
Leonel Godoy <60699907+aka-leonel@users.noreply.github.com>
2022-07-18 13:31:03 -0300

    OT234-51 (#42)
    * se agrega los DAGS para Universidad de Flores y Universidad Nacional de Villa Maria
    
    * Se configuraron los retries para UNVM_dag,py
    
    * Se configuraró el retry para UFlo_dag.py. La estructura del DAG se ajustó un poco más a la propuesta en la documentacion de ejemplo de Airflow 2.2.2
    
    * Se configuraron los logs para UFlo y UNVM
    
    * Se configuro el postgres operator para dags UFlo y UNVM. Se trajeron las consultas SQL requeridas a la carpeta include. Se cambio el nombre de la carpeta'Include' por 'include'
    
    Co-authored-by: Fernando Palacios <fernandogabrielpalacios@gmail.com>

--- airflow/dags/UFlo_dag.py
+++ airflow/dags/UFlo_dag.py
@@ -1,3 +1,4 @@
+
 """
 Descripción
 COMO: Analista de datos
@@ -22,6 +23,32 @@ Nota: A futuro se utilizarán los siguientes modulos
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+import logging
+from airflow.providers.postgres.operators.postgres import PostgresOperator as PO
+
+## Realizar un log al empezar cada DAG con el nombre del logger
+## Formato del log: %Y-%m-%d - nombre_logger - mensaje
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d')
+
+def extract():
+    logging.info('Extract process started.')
+
+def trasnform():
+    logging.info('Transform process started.')
+
+def load():
+    logging.info('Load process started.')
+
+# These args will get passed on to each operator
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5)
+}
 
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
@@ -42,10 +69,15 @@ with DAG(
     schedule_interval=timedelta(hours=1),
     start_date=datetime.today(),
     catchup=False,
-    tags=['example'],
-) as dag:
+    tags=['example']
 
-    extract= DummyOperator(task_id='extract')
+
+) as dag:
+    extract= PO(
+        task_id='extract',
+        postgres_conn_id="postgres_default",
+        sql="../include/UFlo_2020-09-01_2021-02-01_OT234-12.sql"
+        )
     transform= DummyOperator(task_id='transform')
     load= DummyOperator(task_id='load')
 
--- airflow/dags/UNVM_dag.py
+++ airflow/dags/UNVM_dag.py
@@ -1,3 +1,4 @@
+
 """
 Descripción
 COMO: Analista de datos
@@ -21,6 +22,33 @@ Nota: A futuro se utilizarán los siguientes modulos
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+import logging
+from airflow.providers.postgres.operators.postgres import PostgresOperator as PO
+
+# Realizar un log al empezar cada DAG con el nombre del logger
+# Formato del log: %Y-%m-%d - nombre_logger - mensaje
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d')
+
+def extract():
+    logging.info('Extract process started.')
+
+def trasnform():
+    logging.info('Transform process started.')
+
+def load():
+    logging.info('Load process started.')
+
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5)
+}
 
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
@@ -43,10 +71,13 @@ with DAG(
     catchup=False,
     tags=['example'],
 ) as dag:
-
-    extract= DummyOperator(task_id='extract')
-    transform= DummyOperator(task_id='transform')
-    load= DummyOperator(task_id='load')
+    extract = PO(
+        task_id='extract',
+        postgres_conn_id="postgres_default",
+        sql="../include/UNVM_2020-09-01_2021-02-01_OT234-12.sql"
+    )
+    transform = DummyOperator(task_id='transform')
+    load = DummyOperator(task_id='load')
 
     extract >> transform >> load
     
